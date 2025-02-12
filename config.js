61cb02ec1b3b8c238a2a81ae0e87e0a88b8136c0
dineshwpbot <herathmadusankadinesh@gmail.com>
2025-02-12 10:36:00 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "ඔයාගේ session id එක දාන්න",
+SESSION_ID: process.env.SESSION_ID || "vRpmDZpK#632-MTljTqtqx9jbi7gKh4Um42n_Wa1A4-uafWVBMBI",
 // add your Session Id 
 AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN || "true",
 // make true or false status auto seen
