9862d299f73317ad8d764cd0a88e92cc34aed68d
dineshwpbot <herathmadusankadinesh@gmail.com>
2025-02-28 09:44:23 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "vRpmDZpK#632-MTljTqtqx9jbi7gKh4Um42n_Wa1A4-uafWVBMBI",
+SESSION_ID: process.env.SESSION_ID || "eRYH1RaJ#h6R0GFYtfU76yLvonQzioHvFHRuarmlsBn2tYd899aA",
 // add your Session Id 
 AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN || "true",
 // make true or false status auto seen
@@ -27,7 +27,7 @@ CUSTOM_REACT_EMOJIS: process.env.CUSTOM_REACT_EMOJIS || "💝,💖,💗,❤️�
 // chose custom react emojis by yourself 
 DELETE_LINKS: process.env.DELETE_LINKS || "false",
 // automatic delete links witho remove member 
-OWNER_NUMBER: process.env.OWNER_NUMBER || "94724534316",
+OWNER_NUMBER: process.env.OWNER_NUMBER || "94781536595",
 // add your bot owner number
 OWNER_NAME: process.env.OWNER_NAME || "𝐌𝐑 𝐃𝐈𝐍𝐄𝐒𝐇",
 // add bot owner name
